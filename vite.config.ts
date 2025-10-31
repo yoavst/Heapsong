8934aac4aed394b0b67ff3950f640eb035318f1c
Yoav Sternberg <yoav.sternberg@gmail.com>
2025-10-31 18:35:44 +0200

    :green_heart: Remove checks becuase we don't care

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
-        checker({
-            typescript: true,
-            eslint: { lintCommand: 'eslint .', useFlatConfig: true },
-            // overlay: {
-            //     initialIsOpen: 'error',
-            // },
-        }),
     ],
 })
